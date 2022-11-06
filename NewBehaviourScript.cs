bd428467699bda794981b0b82cc64b96f0a87435
artemii <117578468+arturpirojkov330@users.noreply.github.com>
2022-11-06 16:20:05 +0300

    Add files via upload
    ай сей раз сей два сей раз два три

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // bebra
+    void Start()
+    {
+        print("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
